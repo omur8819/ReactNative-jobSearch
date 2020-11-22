9c6fecddabc44ec3fa4762d217b21b15d1af6787
omur8819 <omur8819@gmail.com>
2020-11-23 00:23:05 +0300

    Introduction completed

--- jobSearch/src/pages/Introduction.js
+++ jobSearch/src/pages/Introduction.js
@@ -1,71 +1,72 @@
 import React from 'react';
-import {SafeAreaView, View, Text, ScrollView } from 'react-native';
-
-import { TopicItem } from '../components';
+import {SafeAreaView, View, Text, ScrollView} from 'react-native';
+import {introduction} from '../styles';
+import {TopicItem} from '../components';
 
 const topics = [
   {
     id: 0,
     name: 'Java',
-    color: 'fb5607'
+    color: 'fb5607',
   },
   {
     id: 1,
     name: 'Python',
-    color: '007f5f'
+    color: '007f5f',
   },
   {
     id: 2,
     name: 'Javascript',
-    color: 'ffb703'
+    color: 'ffb703',
   },
   {
     id: 3,
     name: '.NET',
-    color: '023e7d'
+    color: '023e7d',
   },
   {
     id: 4,
     name: 'Dart',
-    color: '001845'
+    color: '001845',
   },
   {
     id: 5,
     name: 'Go',
-    color: 'f8961e'
+    color: 'f8961e',
   },
   {
     id: 6,
     name: 'Ruby',
-    color: 'e63946'
+    color: 'e63946',
   },
   {
     id: 7,
     name: 'C',
-    color: 'fb8b24'
+    color: 'fb8b24',
   },
   {
     id: 8,
     name: 'C++',
-    color: '06d6a0'
+    color: '06d6a0',
   },
-
-]
+];
 
 const Introduction = (props) => {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={{flex: 1}}>
-        <Text>INTRODUCTION</Text>
+  function selectLanguage(lang) {
+    props.navigation.navigate('Jobs', {selectedLanguage: lang});
+  }
 
-        <ScrollView>
-            {
-                topics.map(topic => {
-                return <TopicItem 
-                    key={topic.id} item={topic} 
-                    
-                        />
-            })}
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: '#eceff1'}}>
+      <View style={{flex: 1}}>
+        <View style={introduction.banner}>
+          <Text style={introduction.bannerText}>Aradığınız dili seçin</Text>
+        </View>
+
+        <ScrollView horizontal contentContainerStyle={{alignItems: 'center'}}>
+          {topics.map((t) => {
+            return <TopicItem key={t.id} item={t} onSelect={() => selectLanguage(t.name)} />;
+          })}
         </ScrollView>
       </View>
     </SafeAreaView>
--- jobSearch/src/pages/Jobs.js
+++ jobSearch/src/pages/Jobs.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
 
-const Jobs = () => {
+const Jobs = (props) => {
+    const { selectedLanguage } = props.route.params;
     return (
         <SafeAreaView>
             <View>
-                <Text>JOBS</Text>
+                <Text>{selectedLanguage}</Text>
             </View>
         </SafeAreaView>
     )
