24293303a9413e9e7603decd95777f5d88c659cf
omur8819 <omur8819@gmail.com>
2020-11-23 00:55:30 +0300

    Jobs and JobsItem completed

--- jobSearch/src/pages/Jobs.js
+++ jobSearch/src/pages/Jobs.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, View, FlatList } from 'react-native';
+
+import { JobItem } from '../components';
 
 const Jobs = (props) => {
+    const [data, setData] = useState([]);
     const { selectedLanguage } = props.route.params;
+
+    const fetchData = async () => { 
+        const response = await axios.get(`https://jobs.github.com/positions.json?search=${selectedLanguage.toLowerCase()}`,
+        );
+        setData(response.data);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const renderJobs = ( {item} ) => {
+        return (
+            <JobItem job={item} />
+        )
+    }
+
     return (
         <SafeAreaView>
             <View>
-                <Text>{selectedLanguage}</Text>
+                <Text>JOBS</Text>
+                <FlatList 
+                    data={data}
+                    renderItem={renderJobs}
+                    keyExtractor={(item, index) => index.toString()}
+                />
             </View>
         </SafeAreaView>
     )
