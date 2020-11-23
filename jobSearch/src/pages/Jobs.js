2faec422f5b2e222ef40439535a0c75c6a45b726
omur8819 <omur8819@gmail.com>
2020-11-23 14:38:15 +0300

    Added WebView support for job description

--- jobSearch/src/pages/Jobs.js
+++ jobSearch/src/pages/Jobs.js
@@ -1,8 +1,16 @@
 import Axios from 'axios';
 import Modal from 'react-native-modal';
+import {WebView} from 'react-native-webview';
 import React, {useState, useEffect} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {SafeAreaView, Text, View, FlatList, Button, TouchableOpacity} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
 
 import {jobs} from '../styles';
 import {JobItem} from '../components';
@@ -34,18 +42,17 @@ const Jobs = (props) => {
   );
 
   const onJobSave = async () => {
-    let savedJobList = await AsyncStorage.getItem("@SAVED_JOBS");
-    savedJobList = savedJobList == null ? [] : JSON.parse(savedJobList)
+    let savedJobList = await AsyncStorage.getItem('@SAVED_JOBS');
+    savedJobList = savedJobList == null ? [] : JSON.parse(savedJobList);
 
     const updatedJobList = [...savedJobList, selectedJob];
 
-    AsyncStorage.setItem("@SAVED_JOBS", JSON.stringify(updatedJobList));
-
-  }
+    AsyncStorage.setItem('@SAVED_JOBS', JSON.stringify(updatedJobList));
+  };
 
   return (
     <SafeAreaView style={{flex: 1}}>
-      <View style={{ flex: 1 }}>
+      <View style={{flex: 1}}>
         <Text
           style={{
             textAlign: 'center',
@@ -63,14 +70,15 @@ const Jobs = (props) => {
             borderRadius: 10,
             position: 'absolute',
             bottom: 10,
-            right: 10
+            right: 10,
           }}
-          onPress={() => props.navigation.navigate("SavedJobs")}
-        >
-          <Text style={{color: 'white'}}>See saved jobs</Text>
+          onPress={() => props.navigation.navigate('SavedJobs')}>
+          <Text style={{color: 'white'}}>Kayıtlıları Gör</Text>
         </TouchableOpacity>
 
-        <Modal isVisible={modalFlag} onBackdropPress={() => setModalFlag(false)}>
+        <Modal
+          isVisible={modalFlag}
+          onBackdropPress={() => setModalFlag(false)}>
           <View style={jobs.modalBackground}>
             <View style={{borderBottomWidth: 2, borderColor: '#bdbdbd'}}>
               <Text style={jobs.jobTitle}>{selectedJob.title}</Text>
@@ -80,7 +88,9 @@ const Jobs = (props) => {
               <Text>{selectedJob.company}</Text>
             </View>
             <View style={jobs.jobDesc}>
-              <Text numberOfLines={5}>{selectedJob.description}</Text>
+              <WebView
+                source={{html: selectedJob.description}}
+              />
             </View>
             <Button title="Kaydet" onPress={onJobSave} />
           </View>
